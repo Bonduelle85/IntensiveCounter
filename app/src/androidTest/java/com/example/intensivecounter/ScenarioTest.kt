4c4da77509698de7a7d8b8bb63fffcf1f490a7cc
Anton Gorokhov <bonduelle85@gmail.com>
2024-05-03 13:09:14 +0300

    ScenarioTest - TestCase

---
+++ app/src/androidTest/java/com/example/intensivecounter/ScenarioTest.kt
@@ -0,0 +1,44 @@
+package com.example.intensivecounter
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class ScenarioTest {
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    private fun recreate(){
+        activityScenarioRule.scenario.recreate()
+    }
+
+    /**
+     * TestCase
+     *
+     * 1) Состояние Initial
+     * 2) Нажать на increment (состояние “process”)
+     * 3) Нажать на increment 9 раз (состояние “finish”)
+     * 4) Нажать на reset (состояние “initial”)
+     */
+    @Test
+    fun testCase() {
+        val counterPage = CounterPage()
+        counterPage.checkStateIsInitial()
+
+        counterPage.clickIncrement()
+        counterPage.checkStateIsProcess()
+
+        repeat(9) {
+            counterPage.clickIncrement()
+        }
+        counterPage.checkStateIsFinish()
+
+        counterPage.clickReset()
+        counterPage.checkStateIsInitial()
+    }
+}
