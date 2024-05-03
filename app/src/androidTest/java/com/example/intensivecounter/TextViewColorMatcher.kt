fc992d376a55efb34227abe4d34c57aeef6e5f04
Anton Gorokhov <bonduelle85@gmail.com>
2024-05-03 16:16:39 +0300

    TextViewColorMatcher fixed

--- app/src/androidTest/java/com/example/intensivecounter/TextViewColorMatcher.kt
+++ app/src/androidTest/java/com/example/intensivecounter/TextViewColorMatcher.kt
@@ -1,24 +1,23 @@
 package com.example.intensivecounter
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.test.espresso.matcher.BoundedMatcher
 import org.hamcrest.Description
 
 
-class TextViewColorMatcher(private val color: Int) :
-    BoundedMatcher<View, TextView>(TextView::class.java) {
-
-    constructor(colorString: String) : this(Color.parseColor(colorString))
-
-    override fun matchesSafely(textView: TextView): Boolean {
-        return (textView.currentTextColor as ColorDrawable).color == color
-    }
+class TextViewColorMatcher(private val expectedId: Int) :
+    BoundedMatcher<View?, TextView>(TextView::class.java) {
 
     override fun describeTo(description: Description) {
         description.appendText("with text color: ")
+        description.appendValue(expectedId)
+    }
+
+    override fun matchesSafely(textView: TextView): Boolean {
+        val colorId = ContextCompat.getColor(textView.context, expectedId)
+        return textView.currentTextColor == colorId
     }
 }
 
